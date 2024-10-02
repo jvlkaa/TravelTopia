1ec7c5dada7f465bc9d2a340bcb06c4adbcad682
kalinovsk1 <robert.kali0204@gmail.com>
2024-10-02 18:57:23 +0200

    create trip controller and service

---
+++ backend-app/backend-app/Controllers/TripController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using backend_app.Services;
+using backend_app.Models;
+
+namespace backend_app.Controllers
+{
+    [Route("TravelTopia/[controller]")]
+    [ApiController]
+    public class TripController : Controller
+    {
+        private readonly TripService tripService;
+
+        public TripController(TripService tripService)
+        {
+            this.tripService = tripService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTrip(Trip trip)
+        {
+            await this.tripService.CreateTripAsync(trip);
+            return CreatedAtAction(nameof(CreateTrip), trip);
+            
+        }
+    }
+}
---
+++ backend-app/backend-app/Services/TripService.cs
@@ -0,0 +1,21 @@
+﻿using backend_app.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace backend_app.Services
+{
+    public class TripService
+    {
+        private readonly IMongoCollection<Trip> trips;
+
+        public TripService(IOptions<TravelTopiaDatabaseConfiguration> options)
+        {
+            var mongoClient = new MongoClient(options.Value.ConnectionString);
+            var travelTopiaDatabase = mongoClient.GetDatabase(options.Value.DatabaseName);
+            this.trips = travelTopiaDatabase.GetCollection<Trip>(options.Value.TripCollectionName);
+        }
+
+        public async Task CreateTripAsync(Trip trip) => await trips.InsertOneAsync(trip);
+
+    }
+}
