b31fc5a17ce93dc117ec483fb4c5cf3ba1668005
kalinovsk1 <robert.kali0204@gmail.com>
2024-10-16 15:14:05 +0200

    add controllers for adding and deleting a trip

--- backend-app/backend-app/Controllers/TripController.cs
+++ backend-app/backend-app/Controllers/TripController.cs
@@ -2,6 +2,8 @@
 using backend_app.Services;
 using backend_app.Models;
 using MongoDB.Bson.IO;
+using MongoDB.Driver;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace backend_app.Controllers
 {
@@ -21,20 +23,34 @@ namespace backend_app.Controllers
         {
             await this.tripService.CreateTripAsync(trip);
             return CreatedAtAction(nameof(CreateTrip), trip);
-            
+
         }
 
         [HttpGet]
         public async Task<IActionResult> GetTrips()
         {
             var trips = await tripService.GetTripsAsync();
-            if(trips == null)
+            if (trips == null)
             {
                 return NotFound();
             }
             else
             {
                 return Ok(trips);
+            }
+        }
+
+        [HttpGet("{name}")]
+        public async Task<IActionResult> GetTrip(string name)
+        {
+            var result = await tripService.GetTripAsync(name);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(result);
             }
         }
 
@@ -49,6 +65,20 @@ namespace backend_app.Controllers
             else
             {
                 return Ok(result);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTrip(string id)
+        {
+            var result = await tripService.deleteTripAsync(id);
+            if (result)
+            {
+                return NoContent();
+            }
+            else
+            {
+                return NotFound();
             }
         }
     }
