fbb3f81a7272a02557af9cf079dbb2305e07ed13
kalinovsk1 <robert.kali0204@gmail.com>
2024-12-05 12:02:39 +0100

    return unauthorized if a user token is incorrect and modify buttons in create trip view

--- backend-app/backend-app/Controllers/UserController.cs
+++ backend-app/backend-app/Controllers/UserController.cs
@@ -26,252 +26,369 @@ namespace backend_app.Controllers
 
 
         [HttpPost("login")]
-        public async Task<IActionResult> GoogleLogin([FromBody] string idToken)
+        public async Task<IActionResult> GoogleLogin([FromBody] string token)
         {
-            var payload = await VerifyGoogleToken(idToken);
-            if (payload == null)
-                return BadRequest("Invalid Google token.");
-
-            var user = await userService.GetUserByGoogleIdAsync(payload.Subject);
-            if (user == null)
+            try
             {
-                user = new User
-                {
-                    googleId = payload.Subject,
-                    role = (payload.Email == "traveltopiadeveloper@gmail.com" ? "developer" : "user"),
-                    firstName = payload.GivenName,
-                    lastName = payload.FamilyName,
-                    email = payload.Email,
-                };
-                await userService.CreateUserAsync(user);
-            }
+                var payload = await VerifyGoogleToken(token);
+                var user = await userService.GetUserByGoogleIdAsync(payload.Subject);
 
-            return Ok();
+                if (user == null)
+                {
+                    user = new User
+                    {
+                        googleId = payload.Subject,
+                        role = (payload.Email == "traveltopiadeveloper@gmail.com" ? "developer" : "user"),
+                        firstName = payload.GivenName,
+                        lastName = payload.FamilyName,
+                        email = payload.Email,
+                    };
+
+                    await userService.CreateUserAsync(user);
+                }
+
+                return Ok();
+            }
+            catch (InvalidJwtException ex)
+            {
+                return Unauthorized(new { message = ex.Message });
+            }
         }
 
-        private async Task<GoogleJsonWebSignature.Payload> VerifyGoogleToken(string idToken)
+        private async Task<GoogleJsonWebSignature.Payload> VerifyGoogleToken(string token)
         {
             var settings = new GoogleJsonWebSignature.ValidationSettings()
             {
                 Audience = new List<string> { "594811329058-f3i591f9al5a22i3sbghck3mv4j0ia2h.apps.googleusercontent.com" }
             };
 
-            var payload = await GoogleJsonWebSignature.ValidateAsync(idToken, settings);
+            var payload = await GoogleJsonWebSignature.ValidateAsync(token, settings);
+
             return payload;
         }
 
         [HttpGet("{user}/RouteIds")]
         public async Task<IActionResult> GetRoutesByString(string user)
         {
-            var payload = await VerifyGoogleToken(user);
-            var result = await userService.getRoutesFromUserAsync(payload.Subject);
-            if (result == null)
+            try
             {
-                return NotFound();
+                var payload = await VerifyGoogleToken(user);
+                var result = await userService.getRoutesFromUserAsync(payload.Subject);
+
+                if (result == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return Ok(result);
+                }
             }
-            else
+            catch (InvalidJwtException ex)
             {
-                return Ok(result);
+                return Unauthorized(new { message = ex.Message });
             }
         }
 
         [HttpGet("{user}/routesFilter")]
         public async Task<IActionResult> GetFilteredRoutes(string user, [FromQuery] string? name = null, string? type = null, string? difficulty = null)
         {
-            var payload = await VerifyGoogleToken(user);
-            var userRoutes = await userService.getRoutesFromUserAsync(payload.Subject);
+            try
+            {
+                var payload = await VerifyGoogleToken(user);
+                var userRoutes = await userService.getRoutesFromUserAsync(payload.Subject);
+                var result = await routeService.GetFilteredUserRoutesAsync(userRoutes, name, type, difficulty);
 
-            var result = await routeService.GetFilteredUserRoutesAsync(userRoutes, name, type, difficulty);
-            if (result == null)
-            {
-                return NotFound();
+                if (result == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return Ok(result);
+                }
             }
-            else
+            catch (InvalidJwtException ex)
             {
-                return Ok(result);
+                return Unauthorized(new { message = ex.Message });
             }
         }
 
         [HttpGet("{user}/tripsFilter")]
         public async Task<IActionResult> GetFilteredTrips(string user, [FromQuery] string? name = null, [FromQuery] string? difficulty = null)
         {
-            var payload = await VerifyGoogleToken(user);
-            var userTrips = await userService.getTripsFromUserAsync(payload.Subject);
+            try
+            {
+                var payload = await VerifyGoogleToken(user);
+                var userTrips = await userService.getTripsFromUserAsync(payload.Subject);
+                var result = await tripService.GetFilteredUserTripsAsync(userTrips, name, difficulty);
 
-            var result = await tripService.GetFilteredUserTripsAsync(userTrips, name, difficulty);
-            if (result == null)
-            {
-                return NotFound();
+                if (result == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return Ok(result);
+                }
             }
-            else
+            catch (InvalidJwtException ex)
             {
-                return Ok(result);
+                return Unauthorized(new { message = ex.Message });
             }
         }
 
         [HttpGet("{user}/TripIds")]
         public async Task<IActionResult> GetTripsByString(string user)
         {
-            var payload = await VerifyGoogleToken(user);
-            var result = await userService.getTripsFromUserAsync(payload.Subject);
-            if (result == null)
+            try
             {
-                return NotFound();
+                var payload = await VerifyGoogleToken(user);
+                var result = await userService.getTripsFromUserAsync(payload.Subject);
+
+                if (result == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return Ok(result);
+                }
             }
-            else
+            catch (InvalidJwtException ex)
             {
-                return Ok(result);
+                return Unauthorized(new { message = ex.Message });
             }
         }
 
         [HttpGet("NearUserPoint/{latitude}/{longitude}")]
         public async Task<IActionResult> GetRoutesByPoint(double latitude, double longitude, [FromQuery] List<string> routes)
         {
-            var result = await routeService.GetUserRoutesByPointAsync(routes, latitude, longitude);
-            if (result == null)
+            try
             {
-                return NotFound();
+                var result = await routeService.GetUserRoutesByPointAsync(routes, latitude, longitude);
+
+                if (result == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return Ok(result);
+                }
             }
-            else
+            catch (InvalidJwtException ex)
             {
-                return Ok(result);
+                return Unauthorized(new { message = ex.Message });
             }
         }
 
         [HttpPost("addRoute")]
         public async Task<IActionResult> AddRoute([FromBody] UserIdRouteId data)
         {
-            var payload = await VerifyGoogleToken(data.user);
-            await userService.AddRouteAsync(payload.Subject, data.route);
-            return NoContent();
+            try
+            {
+                var payload = await VerifyGoogleToken(data.user);
+
+                await userService.AddRouteAsync(payload.Subject, data.route);
+
+                return NoContent();
+            }
+            catch (InvalidJwtException ex)
+            {
+                return Unauthorized(new { message = ex.Message });
+            }
         }
 
         [HttpPost("addNewRoute")]
         public async Task<IActionResult> AddNewRoute([FromBody] AddUserRoute route)
         {
-            var payload = await VerifyGoogleToken(route.userIdToken);
-
-            if (await routeService.addUserRoute(route))
+            try
             {
-                var newRoute = await routeService.GetRouteAsync(route.name);
-                await userService.AddRouteAsync(payload.Subject, newRoute.id);
+                var payload = await VerifyGoogleToken(route.userIdToken);
 
-                return NoContent();
+                if (await routeService.addUserRoute(route))
+                {
+                    var newRoute = await routeService.GetRouteAsync(route.name);
+
+                    await userService.AddRouteAsync(payload.Subject, newRoute.id);
+
+                    return NoContent();
+                }
+                else
+                {
+                    return NotFound();
+                }
             }
-            else
+            catch (InvalidJwtException ex)
             {
-                return NotFound();
+                return Unauthorized(new { message = ex.Message });
             }
         }
 
         [HttpPost("addTrip")]
         public async Task<IActionResult> addTrip([FromBody] UserIdTripId data)
         {
-            var payload = await VerifyGoogleToken(data.user);
-            await userService.AddTripAsync(payload.Subject, data.trip);
-            return NoContent();
+            try
+            {
+                var payload = await VerifyGoogleToken(data.user);
+
+                await userService.AddTripAsync(payload.Subject, data.trip);
+
+                return NoContent();
+            }
+            catch (InvalidJwtException ex)
+            {
+                return Unauthorized(new { message = ex.Message });
+            }
         }
 
         [HttpPost("addNewTrip")]
         public async Task<IActionResult> AddNewTrip([FromBody] AddUserTrip trip)
         {
-            var payload = await VerifyGoogleToken(trip.userIdToken);
-
-            if (await tripService.AddUserTrip(trip))
+            try
             {
-                var newTrip = await tripService.GetTripAsync(trip.name);
-                await userService.AddTripAsync(payload.Subject, newTrip.id);
+                var payload = await VerifyGoogleToken(trip.userIdToken);
 
-                return NoContent();
+                if (await tripService.AddUserTrip(trip))
+                {
+                    var newTrip = await tripService.GetTripAsync(trip.name);
+
+                    await userService.AddTripAsync(payload.Subject, newTrip.id);
+
+                    return NoContent();
+                }
+                else
+                {
+                    return NotFound();
+                }
             }
-            else
+            catch (InvalidJwtException ex)
             {
-                return NotFound();
+                return Unauthorized(new { message = ex.Message });
             }
         }
 
         [HttpPost("deleteRoute")]
         public async Task<IActionResult> DeleteRoute([FromBody] UserIdRouteId data)
         {
-            var payload = await VerifyGoogleToken(data.user);
-            await userService.DeleteRouteAsync(payload.Subject, data.route);
-
-            var route = await routeService.GetRouteByIdAsync(data.route);
-
-            if (route.userCreated == true)
+            try
             {
-                var result = await routeService.deleteRouteAsync(route.id);
-                if(result == false)
-                {
-                    return NotFound();
-                }
-            }
+                var payload = await VerifyGoogleToken(data.user);
 
-            return NoContent();
+                await userService.DeleteRouteAsync(payload.Subject, data.route);
+
+                var route = await routeService.GetRouteByIdAsync(data.route);
+
+                if (route.userCreated == true)
+                {
+                    var result = await routeService.deleteRouteAsync(route.id);
+                    if (result == false)
+                    {
+                        return NotFound();
+                    }
+                }
+
+                return NoContent();
+            }
+            catch (InvalidJwtException ex)
+            {
+                return Unauthorized(new { message = ex.Message });
+            }
         }
 
         [HttpPost("deleteTrip")]
         public async Task<IActionResult> DeleteTrip([FromBody] UserIdTripId data)
         {
-            var payload = await VerifyGoogleToken(data.user);
-            await userService.DeleteTripAsync(payload.Subject, data.trip);
-
-            var trip = await tripService.GetTripByIdAsync(data.trip);
-
-            if (trip.userCreated == true) 
+            try
             {
-                var result = await tripService.deleteTripAsync(trip.id);
-                if (result == false)
+                var payload = await VerifyGoogleToken(data.user);
+
+                await userService.DeleteTripAsync(payload.Subject, data.trip);
+
+                var trip = await tripService.GetTripByIdAsync(data.trip);
+
+                if (trip.userCreated == true)
                 {
-                    return NotFound();
+                    var result = await tripService.deleteTripAsync(trip.id);
+                    if (result == false)
+                    {
+                        return NotFound();
+                    }
                 }
+                return NoContent();
             }
-            return NoContent();
+            catch (InvalidJwtException ex)
+            {
+                return Unauthorized(new { message = ex.Message });
+            }
         }
 
         [HttpGet("userRole/{googleId}")]
         public async Task<IActionResult> GetRole(string googleId)
         {
-            var payload = await VerifyGoogleToken(googleId);
-            var result = await userService.GetRoleAsync(payload.Subject);
-            if(result == string.Empty)
+            try
             {
-                return NotFound();
+                var payload = await VerifyGoogleToken(googleId);
+                var result = await userService.GetRoleAsync(payload.Subject);
+
+                if (result == string.Empty)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return Content(result, "text/plain"); //return Ok(result);
+                }
             }
-            else
+            catch (InvalidJwtException ex)
             {
-                return Content(result, "text/plain"); //return Ok(result);
+                return Unauthorized(new { message = ex.Message });
             }
         }
 
         [HttpPost("updateRole/{googleId}")]
         public async Task<IActionResult> UpdateRole(string googleId)
         {
-            var payload = await VerifyGoogleToken(googleId);
-            await userService.UpdateRoleAsync(payload.Subject);
-            return NoContent();
+            try
+            {
+                var payload = await VerifyGoogleToken(googleId);
+                await userService.UpdateRoleAsync(payload.Subject);
+                return NoContent();
+            }
+            catch (InvalidJwtException ex)
+            {
+                return Unauthorized(new { message = ex.Message });
+            }
         }
 
         [HttpGet("getUser/{userToken}")]
         public async Task<IActionResult> GetUser(string userToken)
         {
-            var payload = await VerifyGoogleToken(userToken);
-            var user = await userService.GetUserByGoogleIdAsync(payload.Subject);
-
-            if (user != null)
+            try
             {
-                UserProfileData userProfileData = new UserProfileData
+                var payload = await VerifyGoogleToken(userToken);
+                var user = await userService.GetUserByGoogleIdAsync(payload.Subject);
+
+                if (user != null)
                 {
-                    name = payload.GivenName,
-                    surname = payload.FamilyName,
-                    email = payload.Email
-                };
+                    UserProfileData userProfileData = new UserProfileData
+                    {
+                        name = payload.GivenName,
+                        surname = payload.FamilyName,
+                        email = payload.Email
+                    };
 
-                return Ok(userProfileData);
+                    return Ok(userProfileData);
+                }
+                else
+                {
+                    return NotFound();
+                }
             }
-            else
+            catch (InvalidJwtException ex)
             {
-                return NotFound();
+                return Unauthorized(new { message = ex.Message });
             }
         }
     }
--- backend-app/backend-app/Models/TravelTopiaDatabaseConfiguration.cs
+++ backend-app/backend-app/Models/TravelTopiaDatabaseConfiguration.cs
@@ -4,7 +4,6 @@
     {
         public string ConnectionString {  get; set; }
         public string DatabaseName { get; set; }
-
         public string RouteCollectionName { get; set; }
         public string UserCollectionName { get; set; }
         public string TripCollectionName { get; set; }
